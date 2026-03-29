0b2ca92ab17cb33477d14b0ae272a656f1ccd5a8
Dolapo <checdolzee3@gmail.com>
2026-03-29 17:32:44 +0100

    Started with a quizzes array of objects and quiz generator to generate random quizzes

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const[quizzes, setQuizzes]= useState([
+      {id:1,
+      question: "What planet is known as the red planet",
+      option:["Mars", "Venus", "Saturn", "Jupiter"],
+      answer: "Mars"
+      },
+      {id:2,
+       question: "What is the symbol of Gold",
+       option : ["Go", "Ag", "Au", "Gu" ],
+       answer: "Au" 
+      },
+      {id:3,
+       question: "How many planets do we have",
+       option : ["5", "7", "6", "8"],
+       answer: "7"
+      },
+      {id:4,
+        question: "What year was the world war ?",
+        option: ["1942", "1943", "1944", "1945"],
+        answer: "1945"
+      }
+  ])
+
+  const quizGenerator = (numQuestions= 10) =>{
+      const shuffled = [...quizzes].sort(()=> Math.random() -0.5)
+      setQuizzes(shuffled.slice(0, numQuestions))
+  }
+
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default App
