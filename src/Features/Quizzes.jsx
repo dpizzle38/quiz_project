8f420762c34ade23625d428be2566cea70c4436f
Dolapo <checdolzee3@gmail.com>
2026-04-14 11:57:46 +0100

    deleted the started and show result logic and replaced with a start screen state to facilitate seperation of views where users can read instructions in a seperate view

--- src/Features/Quizzes.jsx
+++ src/Features/Quizzes.jsx
@@ -5,8 +5,7 @@ import Result from './Result';
 const Quizzes = ({quizzes, onShuffle}) => {
     const[answers, setAnswers] = useState([]);
     const[currentIndex, setCurrentIndex] = useState(0);
-    const[showResult, setShowResult] = useState(false)
-    const[started, setStarted] = useState(false);
+    const[screen, setScreen] = useState("start");
     
 
     const handleAnswer = (option)=>{
@@ -24,45 +23,78 @@ const Quizzes = ({quizzes, onShuffle}) => {
     };
 
     const submit = () =>{
-        setShowResult(true)
+        setScreen("result")
     }
 
 
-    if(showResult){
+    if(screen === "result"){
         return <Result quizzes={quizzes} answers={answers}/>    
+        
     }
-    
-    if(!started){
+
+    if(screen === "howto"){
       return(
-        <div className="min-h-screen flex items-center justify-center bg-gray-100 p-4">
-        <div className="bg-white shadow-lg rounded-2xl p-8 text-center max-w-md w-full">
-        <h1 className='text-3xl font-bold mb-4 text-gray-800'>Quiz App</h1>
-        <h2 className='text-2xl font-bold mb-4' >Welcome to the Quiz</h2>
-        <p className='text-gray-500 mb-6'>Test your knowledge with fun questions</p>
-    <button className='w-full bg-blue-500 hover:bg-blue-600 text-white py-3 rounded-lg font-semibold' onClick={()=> {onShuffle(); setStarted(true)}}
-      
-      >Start Quiz</button>
-    </div>
-    </div>
+        <div>
+          <div>
+            <h2>How to play</h2>
+            <ul>
+              <li>Click start quiz to begin</li>
+              <li>Select one answer per question</li>
+              <li>Use Next/Prev to Navigate</li>
+              <li>Submit at the end to see your score</li>
+              </ul>
+              <button
+          onClick={() => setScreen("start")}
+          className="mt-6 w-full bg-blue-500 text-white py-2 rounded-lg"
+        >
+          Back
+        </button>
+            
+          </div>
+        </div>
       )
     }
+    
+
+    if (screen === "start") {
+  return (
+    <div className="text-center mt-10">
+      <h2 className="text-2xl font-bold mb-4">
+        Welcome to the Quiz
+      </h2>
+
+      <div className="space-y-3">
+
+        <button
+          onClick={() => {
+            onShuffle();
+            setAnswers([]);
+            setCurrentIndex(0);
+            setScreen("quiz");
+          }}
+          className="w-full bg-blue-500 text-white py-2 rounded"
+        >
+          Start Quiz
+        </button>
+
+        <button
+          onClick={() => setScreen("howto")}
+          className="w-full bg-gray-300 py-2 rounded"
+        >
+          How To Play
+        </button>
+
+      </div>
+    </div>
+  );
+}
+
+
+    
     return (
      <div className='min-h-screen bg-gray-100 flex items-center justify-center p-4'> 
     <div className='w-full max-w-xl flex-1 md:max-w-xl bg-white shadow-lg rounded-2xl p-4 md:p-6'>
-    <div className="mb-4">
-  <p className="text-sm mb-1">
-    Question {currentIndex + 1} of {quizzes.length}
-  </p>
-
-  <div className="w-full bg-gray-300 dark:bg-gray-700 rounded-full h-2">
-    <div
-      className="bg-blue-500 h-2 rounded-full transition-all duration-300"
-      style={{
-        width: `${((currentIndex + 1) / quizzes.length) * 100}%`,
-      }}
-    ></div>
-  </div>
-</div>
+    
         <Question question={quizzes[currentIndex]} onAnswer ={handleAnswer} selectedAnswer={answers[currentIndex]}/>
         <div className='space-x-2 space-y-4 mt-6 justify-between'>
         <button className="flex-1 px-4 py-2 bg-gray-300 rounded-lg"  disabled = {currentIndex === 0} onClick={()=>decrement()}>Prev</button>
