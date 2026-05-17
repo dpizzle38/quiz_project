2aff3d8357d9d3e697d06cab7e1f86831778d289
Dolapo <checdolzee3@gmail.com>
2026-05-17 22:48:05 +0100

    Just made changes to my button in Question component to make button options appear bright in dark mode for users

--- src/Features/Question.jsx
+++ src/Features/Question.jsx
@@ -7,9 +7,39 @@ const Question = ({question, onAnswer, selectedAnswer, isLocked}) => {
       {question.question}</h2>
       <div className='space-y-2'>
       {question.options.map((option, index)=>(
-        <button className= "w-full max-w-md text-left px-4 py-2 border rounded-lg min-h-[45px] hover:bg-blue-100 transition "key={index} onClick={()=>onAnswer(option)} disabled={isLocked} style={{backgroundColor : selectedAnswer === option ? "lightblue": "white"}}>
-         {option}
-        </button>
+       <button
+       key={index}
+       onClick={() => onAnswer(option)}
+       disabled={isLocked}
+       className={`
+         w-full
+         mb-3
+         p-3
+         rounded-lg
+         border
+         text-left
+         transition-all
+         duration-200
+     
+         ${
+           selectedAnswer === option
+             ? "bg-blue-500 text-white border-blue-500"
+             : `
+               bg-white
+               text-black
+               border-gray-300
+               hover:bg-blue-100
+     
+               dark:bg-gray-700
+               dark:text-white
+               dark:border-gray-600
+               dark:hover:bg-gray-600
+             `
+         }
+       `}
+     >
+       {option}
+     </button> 
         
       ))}
       </div>
@@ -17,5 +47,5 @@ const Question = ({question, onAnswer, selectedAnswer, isLocked}) => {
     
   )
 }
-
+// className= "w-full max-w-md text-left px-4 py-2 border rounded-lg min-h-[45px] hover:bg-blue-100 transition"
 export default Question;
